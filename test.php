a12f3a1b3b2658a78189d028cbb20a0a2a5a29d7
Peter Kostenko <kpvnnov@gmail.com>
2024-01-21 23:25:20 +0000

    test.php

--- test.php
+++ test.php
@@ -1,7 +1,13 @@
 <?php
+//https://career.habr.com/vacancies/1000136678
+ini_set("log_errors", 1);
+ini_set("display_errors", 1);
+ini_set("error_log", "php-test.log");
 
 use FpDbTest\Database;
 use FpDbTest\DatabaseTest;
+use FpDbTest\DatabaseEngine;
+use FpDbTest\Quotation;
 
 spl_autoload_register(function ($class) {
     $a = array_slice(explode('\\', $class), 1);
@@ -12,13 +18,16 @@ spl_autoload_register(function ($class) {
     require_once $filename;
 });
 
-$mysqli = @new mysqli('localhost', 'root', 'password', 'database', 3306);
-if ($mysqli->connect_errno) {
-    throw new Exception($mysqli->connect_error);
-}
+//$mysqli = @new mysqli('localhost', 'root', 'password', 'database', 3306);
+$mysqli = @new mysqli();
+//if ($mysqli->connect_errno) {
+//    throw new Exception($mysqli->connect_error);
+//}
+$quotation = new Quotation();
+$db = new DatabaseEngine($mysqli, $quotation);
 
-$db = new Database($mysqli);
 $test = new DatabaseTest($db);
+
 $test->testBuildQuery();
 
 exit('OK');
